aabd8612bd41b6f8dcc87810164a4ed9f3b32455
joshy <janez.rotman@tum.de>
2024-04-07 20:48:17 +0200

    add additional tests

--- test/test_tokenizer.cc
+++ test/test_tokenizer.cc
@@ -1,3 +1,8 @@
+// todo: (jr) add edge cases
+// todo: (jr) add unicode tests
+// todo: (jr) add ansii escape tests
+// ---------------------------------------------------------------------------
+#include "csmith.h"
 #include "gtest/gtest.h"
 #include "token.h"
 #include "tokenizer.h"
@@ -9,6 +14,9 @@ namespace {
 // ---------------------------------------------------------------------------
 using tt = qcp::TokenType;
 using Token = qcp::Token;
+// ---------------------------------------------------------------------------
+// todo: (jr) " \r ", " \r\n " "\r\n" "\r",
+std::array whiteSpaces{"", " ", "   ", "\t", "\n", " \t ", " \n "};
 // ---------------------------------------------------------------------------
 std::pair<tt, std::string> punctuatorMap[] = {
     {tt::L_SQ_BRKT, "["},
@@ -158,7 +166,6 @@ std::array<std::string, 27> identifierList{
     "a0a0",
 };
 // ---------------------------------------------------------------------------
-// todo: (jr) add edge cases
 std::pair<std::string, std::size_t> iconstList[] = {
     {"0", 0},
     {"1", 1},
@@ -184,6 +191,54 @@ std::pair<std::string, std::size_t> iconstList[] = {
     {"0b111111111111111111111111111111", 0b111111111111111111111111111111},
     {"0XDeadBEEF", 0XDeadBEEF},
     {"0b1101", 0b1101},
+    {"1'234", 1234},
+    {"0'123", 83}, // Assuming octal based on leading 0
+    {"0x1'2A3F", 0x12A3F},
+    {"0XDE'ADBE'EF", 0xDEADBEEF},
+    {"0b10'1010", 0b101010},
+    {"4'294'967'295", 4294967295}, // Maximum unsigned 32-bit integer
+    {"2'147'483'647", 2147483647}, // Maximum signed 32-bit integer
+};
+// ---------------------------------------------------------------------------
+std::string invalidIconstList[] = {
+    "0x",
+    "0b",
+    "0xG",
+    "0b2",
+    "0b12",
+    "1'a",
+    "0xZ123",
+    "0b102",
+    "08",
+    "123'",
+    "123'456'789'012'345'678'901'234",
+    "0x",
+    "0b",
+    "0xGHIJ",
+};
+// ---------------------------------------------------------------------------
+std::array<std::string, 19> stringLiteralList{
+    "\"Simple string\"",
+    "\"\"", // Empty string
+    "\"single quote (')\"",
+    "\"double quote (\\\")\"",
+    "\"backslash (\\\\)\"",
+    "\"tab (\\t)\"",
+    "\"newline (\\n)\"",
+    "\"carriage return (\\r)\"",
+    "\"escape sequence (\\x1B[31m) for red color\"",
+    {"\"embedded (\\0) null in text\"", 28},
+    // "\"unicodes (\\u03A9 \\u03C0)\"",
+    "\"hex escape (\\x48\\x65\\x6C\\x6C\\x6F)\"",
+    "\"octal escape (\\1\\12\\110\\145\\154\\154\\157)\"",
+    "\"escape sequence (\\n\\t\\\'\\\"\\\\)\"",
+    "\"\\\"starts and ends with quote\\\"\"",
+    "\"\\\'String starts and ends with a single quote but is not char sequence\\\'\"",
+    "\"a /* block comment */ inside\"",
+    "\"a // line comment inside\"",
+    "\"`backticks`\"",
+    "\"special characters $!@#%^&*()-+=[]{}|;:,.<>?/\"",
+    // "\"non-standard escape \\e for ASCII escape \"",
 };
 // ---------------------------------------------------------------------------
 /*template <std::size_t len>
@@ -241,49 +296,59 @@ class IdentTT : public ::testing::TestWithParam<std::string> {
 class IConstTT : public ::testing::TestWithParam<std::pair<std::string, std::size_t>> {
 };
 // ---------------------------------------------------------------------------
+class InvalidIConstTT : public ::testing::TestWithParam<std::string> {
+};
+// ---------------------------------------------------------------------------
+class StringLiteralTT : public ::testing::TestWithParam<std::string> {
+};
+// ---------------------------------------------------------------------------
 TEST_P(TT, GetsRecognized) {
    auto [token, word] = GetParam();
-   std::array whiteSpaces{"", " ", "   ", "\t", "\n", "\r", "\r\n", " \t ", " \n ", " \r ", " \r\n "};
    for (auto& prefix : whiteSpaces) {
       for (auto& suffix : whiteSpaces) {
          std::string src{prefix + word + suffix};
          qcp::Tokenizer ts{src};
          auto it = ts.begin();
-         ASSERT_EQ(it->getType(), token);
-         ASSERT_EQ(++it, ts.end());
+         ASSERT_EQ(it->getType(), token) << "Input string: " << std::quoted(src);
+         ASSERT_EQ(++it, ts.end()) << "Input string: " << std::quoted(src);
       }
    }
 }
 // ---------------------------------------------------------------------------
 TEST_P(IdentTT, GetsRecognized) {
    auto word = GetParam();
-   std::array whiteSpaces{"", " ", "   ", "\t", "\n", "\r", "\r\n", " \t ", " \n ", " \r ", " \r\n "};
    for (auto& prefix : whiteSpaces) {
       for (auto& suffix : whiteSpaces) {
-         std::string src{prefix + word + suffix};
+         std::string src{prefix};
+         src += word;
+         src += suffix;
          qcp::Tokenizer ts{src};
          auto it = ts.begin();
-         ASSERT_EQ(it->getType(), tt::IDENT);
-         ASSERT_EQ(++it, ts.end());
+         ASSERT_EQ(it->getType(), tt::IDENT) << "Input string: " << std::quoted(src);
+         ASSERT_EQ(++it, ts.end()) << "Input string: " << std::quoted(src);
       }
    }
 }
 // ---------------------------------------------------------------------------
 template <typename T>
+void test_iconst(const std::string& word, const std::vector<std::string>& suffixes, tt type, T value);
+// ---------------------------------------------------------------------------
+template <typename T>
 void test_iconst(const std::string& word, const std::vector<std::string>& suffixes, tt type, T value) {
-   std::array whiteSpaces{"", " ", "   ", "\t", "\n", "\r", "\r\n", " \t ", " \n ", " \r ", " \r\n "};
    for (auto& suffix : suffixes) {
       for (auto& wsPrefix : whiteSpaces) {
          for (auto& wsSuffix : whiteSpaces) {
-            std::string src{wsPrefix + word + suffix + wsSuffix};
+            std::string src{wsPrefix};
+
+            src += word;
+            src += suffix;
+            src += wsSuffix;
+
             qcp::Tokenizer ts{src};
             auto it = ts.begin();
-            if (it->getType() != type) {
-               std::cerr << std::quoted(wsPrefix + word + suffix + wsSuffix) << std::endl;
-            }
-            ASSERT_EQ(it->getType(), type);
-            ASSERT_EQ(it->getValue<T>(), value);
-            ASSERT_EQ(++it, ts.end());
+            ASSERT_EQ(it->getType(), type) << "Input string: " << std::quoted(src);
+            ASSERT_EQ(it->getValue<T>(), value) << "Input string: " << std::quoted(src);
+            ASSERT_EQ(++it, ts.end()) << "Input string: " << std::quoted(src);
          }
       }
    }
@@ -308,10 +373,58 @@ TEST_P(IConstTT, GetsRecognized) {
    // todo: (jr) wb, uwb
 }
 // ---------------------------------------------------------------------------
+TEST_P(StringLiteralTT, GetsRecognized) {
+   auto word = GetParam();
+   std::array whiteSpaces{"", " ", "   ", "\t", "\n", "\r", "\r\n", " \t ", " \n ", " \r ", " \r\n "};
+   for (auto& prefix : whiteSpaces) {
+      for (auto& suffix : whiteSpaces) {
+         std::string src{prefix + word + suffix};
+         qcp::Tokenizer ts{src};
+         auto it = ts.begin();
+         ASSERT_EQ(it->getType(), tt::LITERAL);
+         ASSERT_EQ(it->getValue<std::string_view>(), word.substr(1, word.size() - 2));
+         ASSERT_EQ(++it, ts.end());
+      }
+   }
+}
+// ---------------------------------------------------------------------------
+TEST_P(InvalidIConstTT, invalidIConstNotParsed) {
+   auto word = GetParam();
+   for (auto& prefix : whiteSpaces) {
+      for (auto& suffix : whiteSpaces) {
+         std::string src{prefix};
+         src += word;
+         src += suffix;
+         ASSERT_EXIT({
+            qcp::Tokenizer ts{src};
+            (void) ts.begin();
+         },
+                     testing::KilledBySignal(6), ".*");
+      }
+   }
+}
+// ---------------------------------------------------------------------------
 INSTANTIATE_TEST_CASE_P(Punctuator, TT, ::testing::ValuesIn(punctuatorMap));
 INSTANTIATE_TEST_CASE_P(Keyword, TT, ::testing::ValuesIn(keywordMap));
 INSTANTIATE_TEST_CASE_P(Identifier, IdentTT, ::testing::ValuesIn(identifierList));
 INSTANTIATE_TEST_CASE_P(IntegerConstant, IConstTT, ::testing::ValuesIn(iconstList));
+INSTANTIATE_TEST_CASE_P(InvalidIntegerConstant, InvalidIConstTT, ::testing::ValuesIn(invalidIconstList));
+INSTANTIATE_TEST_CASE_P(StringLiteral, StringLiteralTT, ::testing::ValuesIn(stringLiteralList));
+// ---------------------------------------------------------------------------
+TEST(Tokenizer, Random) {
+   std::string src{qcp::tool::random_c_program("gcc", 0)};
+   qcp::Tokenizer ts{src};
+   std::ofstream ofs{"out.log"};
+   std::cout << "Random test started" << std::endl;
+   for (auto token : ts) {
+      ASSERT_TRUE(token.getType() != tt::UNKNOWN);
+      ofs << token << ' ';
+      if (token.getType() == tt::SEMICOLON) {
+         ofs << std::endl;
+      }
+   }
+   std::cout << "Random test passed" << std::endl;
+}
 // ---------------------------------------------------------------------------
 } // namespace
 // ---------------------------------------------------------------------------
