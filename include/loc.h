124e0d7d5f939ed4883f2464fd3643b88779dd01
joshy <janez.rotman@tum.de>
2024-05-27 09:16:26 +0200

    fix bug in srcloc calculation

--- include/loc.h
+++ include/loc.h
@@ -4,6 +4,7 @@
 // qcp
 // ---------------------------------------------------------------------------
 #include <algorithm>
+#include <string_view>
 // ---------------------------------------------------------------------------
 namespace qcp {
 // ---------------------------------------------------------------------------
@@ -11,18 +12,26 @@ struct SrcLoc {
    std::size_t loc;
    unsigned len;
 
+   using sv_it = typename std::string_view::const_iterator;
+
    SrcLoc() : loc{0}, len{0} {}
    SrcLoc(std::size_t loc, unsigned len) : loc{loc}, len{len} {}
    SrcLoc(std::size_t loc, std::size_t locEnd) : loc{loc}, len{static_cast<unsigned int>(locEnd - loc)} {}
+   SrcLoc(sv_it progBegin, sv_it begin, sv_it end) : loc{static_cast<std::size_t>(begin - progBegin)}, len{static_cast<unsigned int>(end - begin)} {}
 
    std::size_t locEnd() const {
       return loc + len;
    }
 
    // idea from aengelke
-   SrcLoc operator|(const SrcLoc& other) {
+   SrcLoc operator|(const SrcLoc& other) const {
       std::size_t newLoc{std::min(loc, other.loc)};
       return {newLoc, std::max(locEnd(), static_cast<unsigned int>(other.locEnd()) - newLoc)};
+   }
+
+   SrcLoc& operator|=(const SrcLoc& other) {
+      *this = *this | other;
+      return *this;
    }
 };
 // ---------------------------------------------------------------------------
