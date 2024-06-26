46e9e0c63ce3bb092e742e4f49ad9182bf768978
joshy <janez.rotman@tum.de>
2024-06-26 16:49:24 +0200

    update gperf command

--- gperf.cmake
+++ gperf.cmake
@@ -1,9 +1,9 @@
 find_program(GPERF_EXECUTABLE NAMES gperf)
 
-add_custom_command(OUTPUT "${CMAKE_SOURCE_DIR}/include/keywords.h"
-    COMMAND ${GPERF_EXECUTABLE} --output-file=${CMAKE_SOURCE_DIR}/include/keywords.h ${CMAKE_SOURCE_DIR}/include/keywords.gperf)
+add_custom_command(OUTPUT "${CMAKE_SOURCE_DIR}/src/keywords.cc"
+    COMMAND ${GPERF_EXECUTABLE} --output-file=${CMAKE_SOURCE_DIR}/src/keywords.cc ${CMAKE_SOURCE_DIR}/src/keywords.gperf)
 
 add_custom_target(generate_keyword_hashmap ALL
-    DEPENDS "${CMAKE_SOURCE_DIR}/include/keywords.h")
+    DEPENDS "${CMAKE_SOURCE_DIR}/src/keywords.cc")
 
-install(FILES ${CMAKE_SOURCE_DIR}/include/keywords.h DESTINATION include)
+install(FILES ${CMAKE_SOURCE_DIR}/src/keywords.cc DESTINATION src)
