c28e5f736eb7ad7e9236d1129a2a922ebd01ac8e
joshy <janez.rotman@tum.de>
2024-07-09 12:25:34 +0200

    refractor

--- include/emittertraits.h
+++ include/emittertraits.h
@@ -1,9 +1,8 @@
-#ifndef QCP_EMITTERTRAITS_H
-#define QCP_EMITTERTRAITS_H
+#ifndef QCPTRAITS_H
+#define QCPTRAITS_H
 // ---------------------------------------------------------------------------
 // qcp
 // ---------------------------------------------------------------------------
-#include "scopeinfo.h"
 #include "type.h"
 // ---------------------------------------------------------------------------
 #include <variant>
@@ -11,21 +10,20 @@
 namespace qcp {
 namespace emitter {
 // ---------------------------------------------------------------------------
-template <typename _EmitterT>
+template <typename T>
 struct emitter_traits {
-   using ssa_t = typename _EmitterT::ssa_t;
-   using const_t = typename _EmitterT::const_t;
-   using iconst_t = typename _EmitterT::iconst_t;
-   using phi_t = typename _EmitterT::phi_t;
-   using bb_t = typename _EmitterT::bb_t;
-   using ty_t = typename _EmitterT::ty_t;
-   using fn_t = typename _EmitterT::fn_t;
-   using sw_t = typename _EmitterT::sw_t;
+   using ssa_t = typename T::ssa_t;
+   using const_t = typename T::const_t;
+   using iconst_t = typename T::iconst_t;
+   using phi_t = typename T::phi_t;
+   using bb_t = typename T::bb_t;
+   using ty_t = typename T::ty_t;
+   using fn_t = typename T::fn_t;
+   using sw_t = typename T::sw_t;
 
-   using value_t = std::variant<ssa_t*, const_t*, iconst_t*, fn_t*>;
-   using const_or_iconst_t = std::variant<const_t*, iconst_t*>;
-   using Type = type::Type<_EmitterT>;
-   using ScopeInfo = ScopeInfo<_EmitterT>;
+   using value_t = std::variant<std::monostate, ssa_t*, const_t*, iconst_t*, fn_t*>;
+   using const_or_iconst_t = std::variant<std::monostate, const_t*, iconst_t*>;
+   using Type = type::Type<T>;
 };
 // ---------------------------------------------------------------------------
 } // namespace emitter
