0b0d07a8e58080b8218162f034dc14870efa99b1
joshy <janez.rotman@tum.de>
2024-07-09 12:23:13 +0200

    change scopeinfo to hold arbitrary value

--- include/scopeinfo.h
+++ include/scopeinfo.h
@@ -5,36 +5,30 @@
 // ---------------------------------------------------------------------------
 #include "emittertraits.h"
 #include "loc.h"
+#include "type.h"
 // ---------------------------------------------------------------------------
 #include <variant>
 // ---------------------------------------------------------------------------
 namespace qcp {
+namespace scope {
 // ---------------------------------------------------------------------------
-#define VARIANT_ACCESS_METHODS(name, type, member) \
-   type& name() { return std::get<type>(data_); }  \
-   const type& name() const { return std::get<type>(data_); }
-// ---------------------------------------------------------------------------
-namespace emitter {
-template <typename _EmitterT>
-struct emitter_traits;
-}
-// ---------------------------------------------------------------------------
-template <typename _EmitterT>
+template <typename T>
 class ScopeInfo {
-   using trait = emitter::emitter_traits<_EmitterT>;
+   using trait = emitter::emitter_traits<T>;
 
-   using Type = typename trait::Type;
    using ssa_t = typename trait::ssa_t;
    using fn_t = typename trait::fn_t;
    using iconst_t = typename trait::iconst_t;
    using value_t = typename trait::value_t;
 
+   using Ty = type::Type<T>;
+
    public:
    ScopeInfo() = default;
-   ScopeInfo(Type ty, SrcLoc loc) : ty{ty}, loc{loc}, data_{static_cast<ssa_t*>(nullptr)} {}
+   ScopeInfo(Ty ty, SrcLoc loc) : ty{ty}, loc{loc}, data_{static_cast<ssa_t*>(nullptr)} {}
 
-   template <typename T>
-   ScopeInfo(Type ty, SrcLoc loc, T t, bool hasDefOrInit) : ty{ty}, loc{loc}, hasDefOrInit{hasDefOrInit}, data_{t} {}
+   template <typename U>
+   ScopeInfo(Ty ty, SrcLoc loc, U u, bool hasDefOrInit) : ty{ty}, loc{loc}, hasDefOrInit{hasDefOrInit}, data_{u} {}
 
    // copy and move operations
    ScopeInfo(const ScopeInfo& other) = default;
@@ -42,23 +36,29 @@ class ScopeInfo {
    ScopeInfo& operator=(const ScopeInfo& other) = default;
    ScopeInfo& operator=(ScopeInfo&& other) noexcept = default;
 
-   Type ty;
+   Ty ty;
    SrcLoc loc;
    bool hasDefOrInit;
 
-   template <typename T>
-   bool is() const {
-      return std::holds_alternative<T>(data_);
-   }
+   #define VARIANT_ACCESS_METHODS(name, type, member) \
+      type &name() { return std::get<type>(data_); }  \
+      const type &name() const { return std::get<type>(data_); }
 
    VARIANT_ACCESS_METHODS(ssa, ssa_t*, data_)
    VARIANT_ACCESS_METHODS(fn, fn_t*, data_)
    VARIANT_ACCESS_METHODS(iconst, iconst_t*, data_)
 
+   #undef VARIANT_ACCESS_METHODS
+
+   value_t data() const {
+      return data_;
+   }
+
    private:
    value_t data_;
 };
 // ---------------------------------------------------------------------------
+} // namespace scope
 } // namespace qcp
 // ---------------------------------------------------------------------------
 #endif
